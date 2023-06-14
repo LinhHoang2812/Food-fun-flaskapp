17a22dce5f3421b8c3f7a7b0d908da5c890cde5c
LinhHoang2812 <zoeylinh2812@gmail.com>
2023-06-14 17:35:34 +0200

    change

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ secret_key = os.getenv("SECRET_KEY")
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+app.config['SECRET_KEY'] = f"{secret_key}"
 
 
 ## create database
